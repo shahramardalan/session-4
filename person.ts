522704bd9dbd65db34655a0e0c9cc1ea1eaef990
shahram <ardalan.shahram@gmail.com>
2023-09-15 13:07:09 +0330

    write person app

---
+++ person.ts
@@ -0,0 +1,29 @@
+const person: object = {
+  id: 0,
+  firstName: "",
+  mobile: "",
+};
+const personList: string[] = [];
+
+function addPerson(id: number, firstName: string, mobile: string) {
+  const pr = Object.create(person);
+  pr.id = id;
+  pr.firstName = firstName;
+  pr.mobile = mobile;
+
+  personList.push(pr);
+}
+
+addPerson(365, "shahram", "09331815199");
+addPerson(442, "maryam", "080180182");
+function printPerson() {
+  if (!personList[0]) {
+    return;
+  }
+  console.log(personList);
+}
+printPerson();
+
+let result = personList.find((item) => item.firstName === "roza");
+if (result) console.log("find person");
+else console.log("can't find person");
